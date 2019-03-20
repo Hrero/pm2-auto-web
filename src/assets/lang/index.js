b7ea263767fc075bcb10cdec8d0033ece713da76
shiyi <shiyi@zugeliang01.com>
2019-03-20 14:05:38 +0800

    no message

---
+++ src/assets/lang/index.js
@@ -0,0 +1,6 @@
+import ZHCN from './zh-CN'
+import ENUS from './en-US'
+
+const zhCN = ZHCN
+let enUS = ENUS
+export { zhCN, enUS }
--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueI18n from 'vue-i18n'
+import { zhCN } from './assets/lang'
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
+const i18n = new VueI18n({locale: 'zhCN', messages: { zhCN }})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  i18n,
+  components: {
+    App
+  },
   template: '<App/>'
 })

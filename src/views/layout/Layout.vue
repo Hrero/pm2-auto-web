c073ffd830dbf4dd677197b91c89bfa25a1797d1
shiyi <shiyi@zugeliang01.com>
2019-03-20 19:55:26 +0800

    no message

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-layout id="components-layout-demo-top" class="layout">
+    <a-layout-header>
+      <Navbar/>
+    </a-layout-header>
+    <a-layout-content>
+      <section class="app-main">
+        <transition name="fade-transform">
+          <router-view/>
+        </transition>
+      </section>
+    </a-layout-content>
+    <a-layout-footer>
+      <Footer/>
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import Footer from './Footer'
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style lang="less">
+@import "../../assets/less/reset";
+</style>

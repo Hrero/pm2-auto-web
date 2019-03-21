f76e0f5a8ba27d1c987756c190f072adcb0c1051
shiyi <shiyi@zugeliang01.com>
2019-03-21 09:23:18 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -2,21 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
 import VueI18n from 'vue-i18n'
 import zhCN from './assets/lang/zh-CN'
-import Antd from 'ant-design-vue'
+// import Antd from 'ant-design-vue'
 // import 'ant-design-vue/dist/antd.css'
 
 Vue.use(VueI18n)
-Vue.use(Antd)
+// Vue.use(Antd)
 Vue.config.productionTip = false
 const i18n = new VueI18n({locale: 'zhCN', messages: { zhCN }})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  // router,
   i18n,
   components: {
     App
